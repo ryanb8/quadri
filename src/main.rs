369613160e5033e6edd61d6a49fdaff9bc1b4ae8
Ryan Boyer <ryanboyerpublic@gmail.com>
2022-01-14 18:34:56 -0600

    Generalization

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-static allowed_attribute_values: [i8; 2] = [0, 1];
+static ALLOWED_ATTRIBUTE_VALUES: [i8; 2] = [0, 1];
 
 #[derive(Debug)]
 struct AV(i8);  //allowed values
@@ -8,7 +8,7 @@ struct AV(i8);  //allowed values
 impl AV {
     fn in_allowed_values(value: i8) -> bool {
         let mut is_valid = false;
-        for av in allowed_attribute_values {
+        for av in ALLOWED_ATTRIBUTE_VALUES {
                 if value == av {
                     is_valid = true;
                 }
@@ -28,41 +28,48 @@ impl AV {
 #[derive(Debug)]
 struct GamePiece {
     name: String,
-    at1: i8,
-    at2: i8,
-    at3: i8,
-    at4: i8,
+    ats: Vec<i8>,
+    dim: i8
 }
 
 impl GamePiece {
     fn new(values: &[i8], name: String) -> Result<GamePiece, String> {
-        if values.len() != 4 {
+        let dim: i8 = values.len().try_into().unwrap();
+        if dim != 4 {
             return Err("Need 4 values for a piece".to_string());
         }
         for v in values {
             if !AV::in_allowed_values(*v) {
-                return Err(format!("Values for pieces must be in {:?}", allowed_attribute_values));
+                return Err(format!("Values for pieces must be in {:?}", ALLOWED_ATTRIBUTE_VALUES));
             }
         }
         Ok(GamePiece {
             name: name,
-            at1: values[0],
-            at2: values[1],
-            at3: values[2],
-            at4: values[3]
+            ats: values.to_vec(),
+            dim: dim
         })
+    }
+    fn get_values(&self) -> &Vec<i8> {
+        &self.ats
     }
 }
 
-fn all_equal(arr: &[i8; 4]) -> Result<bool, String> {
+
+#[derive(Debug)]
+struct Gameboard {
+    arrangement: Vec<Vec<Option<GamePiece>>>
+}
+
+
+fn all_equal(arr: Vec<i8>) -> Result<bool, String> {
     let arr_len = arr.len();
     if arr_len == 0 {
         return Err("Array must be non-empty".to_string());
     }
     let mut result = true;
     let mut prior_value = arr[0];
-    for i in 1..arr_len {
-        let next_value = arr[i];
+    for ix in 1..arr_len {
+        let next_value = arr[ix];
         if prior_value != next_value {
             result = false;
             break;
@@ -72,13 +79,25 @@ fn all_equal(arr: &[i8; 4]) -> Result<bool, String> {
     Ok(result)
 }
 
-fn pieces_are_quadri(p1: &GamePiece, p2: &GamePiece, p3: &GamePiece, p4: &GamePiece) -> bool {
-    let l1 = [p1.at1, p2.at1, p3.at1, p4.at1];
-    let l2 = [p1.at2, p2.at2, p3.at2, p4.at2];
-    let l3 = [p1.at3, p2.at3, p3.at3, p4.at3];
-    let l4 = [p1.at4, p2.at4, p3.at4, p4.at4];
 
-    all_equal(&l1).unwrap() || all_equal(&l2).unwrap() || all_equal(&l3).unwrap() || all_equal(&l4).unwrap()
+//assume all pieces have same dimensions
+fn pieces_are_quadri(pieces: Vec<&GamePiece>) -> Result<bool, String> {
+    let number_of_pieces = pieces.len();
+    if number_of_pieces == 0 {
+        return Err("Must provide at least one piece".to_string());
+    }
+    let piece_dimension = pieces[0].dim;
+    // let mut this_check = Vec::new();
+    for jx in 0..piece_dimension as usize{
+        let mut this_check = Vec::new();
+        for ix in 0..number_of_pieces as usize {
+            this_check.push(pieces[ix].ats[jx]);
+        }
+        if all_equal(this_check)? {
+            return Ok(true);
+        }
+    }
+    Ok(false)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -88,11 +107,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let piece4 = GamePiece::new(&[1,0,1,1], "tall_square_dark_full".to_string())?;
     let piece5 = GamePiece::new(&[0,1,1,1], "short_round_dark_full".to_string())?;
 
-    let a_1_4 = pieces_are_quadri(&piece1, &piece2, &piece3, &piece4);
-    let a_2_5 = pieces_are_quadri(&piece5, &piece2, &piece3, &piece4);
+    let a_1_4_p = vec![&piece1, &piece2, &piece3, &piece4];
+    let a_2_5_p = vec![&piece5, &piece2, &piece3, &piece4];
 
-    println!("Pieces 1 - 4 are quadri: {}", a_1_4);
-    println!("Pieces 2- 5 are quadri: {}", a_2_5);
+    let a_1_4 = pieces_are_quadri(a_1_4_p)?;
+    let a_2_5 = pieces_are_quadri(a_2_5_p)?;
+
+    println!("Pieces 1 - 4 are quadri: {:?}", a_1_4);
+    println!("Pieces 2- 5 are quadri: {:?}", a_2_5);
 
     Ok(())
 }
